67729a0ca1efb0c370bc5caa95f5eee5784a4d56
Ginja <virginija.meilune@gmail.com>
2022-08-23 18:56:26 +1000

    Preparing JS for function applications

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+const showModalContainer = document.getElementById('show-modal');
 const bookmark = document.getElementById('bookmark');
 const bookmarkExit = document.getElementById('delete-bookmark');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('close-modal');
+const websiteName = document.getElementById('website-name');
+const websiteURL = document.getElementById('website-url');
+
+
